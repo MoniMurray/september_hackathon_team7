cd21f0ed7aefcf0854b33cba90e0a27d9d35b3fe
Monica Murray <109610326+MoniMurray@users.noreply.github.com>
2023-09-08 22:45:22 +0100

    Update settings.py
    Revert line 32 to previous code to keep SECRET_KEY value private

--- wastewizeweb/settings.py
+++ wastewizeweb/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "yj6gmxa%5&31vbgob08tr*1dw_!8kh95vv1v9kbfvtumdi#=rn")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
