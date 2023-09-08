c0ae037c814b5434f21383a9404e652b174b7ffd
Monica Murray <109610326+MoniMurray@users.noreply.github.com>
2023-09-08 12:07:15 +0100

    Update settings.py
    restore deleted SECRET_KEY variable to remove value and maintain security

--- wastewizeweb/settings.py
+++ wastewizeweb/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "0d6(zjd47(6#mwh6agkr)f!*t#1h%ai(%db!*ap1z!-_0ey$4+")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
