06f0fa45b4957cb06ad6c551f7807707ac8499d2
Aiga <aiga.andrejeva@gmail.com>
2023-09-09 15:38:46 +0100

    Shop cards (#65)
    * Render items from database to shop cards
    
    * Add logo-image and render on main page
    
    * Add locations and category list dropdown

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeShopList.as_view(), name='home'),
+    # path('', views.CategoryList.as_view(), name='home'),
+    # path('', views.LocationList.as_view(), name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,57 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import generic, View
-from shop_directory.models import Shop
+from shop_directory.models import Shop, Category, Location
 
 from django.shortcuts import render
 
 # Create your views here.
 
 
+# def shop_list(request):
+#     shop = Shop.objects.all()
+#     category = Category.objects.all()
+#     location = Location.objects.all()
+#     info = zip(shop, category, location)
+#     context = {'info': info, }
+#     render(request, 'home.html', context)
+
+# class HomeShopList(generic.ListView):
+#     context_object_name = 'home'
+#     template_name = 'home.html'
+#     queryset = Shop.objects.all()
+
+#     def get_context_data(self, **kwargs):
+#         context = super(HomeShopList, self).get_context_data(**kwargs)
+#         context['category'] = Category.objects.all()
+#         context['location'] = Location.objects.all()
+
+#         return context
+
+
+# class HomeShopList(generic.ListView):
+#     model = Shop
+#     template_name = 'home.html'
+
+
 class HomeShopList(generic.ListView):
-    model = Shop
     template_name = 'home.html'
+    context_object_name = 'category_and_location'
+    model = Shop
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeShopList, self).get_context_data(**kwargs)
+        context.update({
+            'category_list': Category.objects.all(),
+            'locations_list': Location.objects.all(),
+            'shop_list': Shop.objects.all(),
+        })
+        return context
+
+    def get_queryset(self):
+        return Shop.objects.all()
+
+
+# class CategoryList(generic.ListView):
+#     model = Category
+#     template_name = 'home.html'
