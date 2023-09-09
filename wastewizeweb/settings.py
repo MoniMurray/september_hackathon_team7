949f4ce43b49817fb2979aad5b7471891bc7bebb
ObiOne84 <stepienszymon@hotmail.com>
2023-09-09 09:06:07 +0000

    Add rates to calculator

--- wastewizeweb/settings.py
+++ wastewizeweb/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "")
+SECRET_KEY = os.environ.get("SECRET_KEY", "@x-2elu%*lh$%huf0y7wi-e_8t4vgl5mp37y@b8+g5f*l-0=y#")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
